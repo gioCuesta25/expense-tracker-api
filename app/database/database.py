5c034b6caf678d59d4238f53574ffde03955263f
Giovanni Romaña <giovanni.cuesta@t-evolvers.com>
2023-10-12 21:45:15 -0500

    Config env variables

--- app/database/database.py
+++ app/database/database.py
@@ -2,16 +2,16 @@ from sqlmodel import create_engine, SQLModel
 import database.models.amounts
 import database.models.categories
 import database.models.user
+from decouple import config
 
-DB_HOST = "peanut.db.elephantsql.com"
-DB_PORT = 5432
-DB_USER = "wijoudvp"
-DB_NAME = "wijoudvp"
-DB_PASSWORD = "b3rtaYTtaJmiMkC7Nuy9O3Az6cOaTM9T"
+DB_HOST = config("DB_HOST")
+DB_PORT = config("DB_PORT")
+DB_USER = config("DB_USER")
+DB_NAME = config("DB_NAME")
+DB_PASSWORD = config("DB_PASSWORD")
 
 POSTGRESQL_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-# POSTGRESQL_URL = 'postgresql://expense_tracker_db_x2d1_user:JtKqcniVo8l7uchhOTjxWWhlghpaP2e6@dpg-ckhmchcldqrs73bsb6ag-a.oregon-postgres.render.com/expense_tracker_db_x2d1'
 
 engine = create_engine(POSTGRESQL_URL, echo=True)
 
